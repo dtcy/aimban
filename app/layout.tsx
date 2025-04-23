a5e4d4448e8ce8e363b3ef2fea4f169e87e1ab7d
seyoi <seyoi@example.com>
2025-04-23 13:54:18 +0900

    asdf;

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex min-h-screen flex-col">
-        <Header />
+        {/* <Header /> */}
         <main className="flex-1">{children}</main>
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   )
